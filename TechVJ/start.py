8c29527464e7774aebbea029c33720242e245dd2
NextGenBotzORG <nextgenbotz.org@gmail.com>
2025-04-23 14:51:21 +0530

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -1,3 +1,4 @@
+```python
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
@@ -399,3 +400,4 @@ def get_message_type(msg: pyrogram.types.messages_and_media.message.Message):
         return "Text"
     except:
         pass
+```
